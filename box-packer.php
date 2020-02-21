b0e02e380add883fe968434e0cde123bc6d50a2a
Duncan H <duncan.haran@gmail.com>
2020-02-21 15:41:17 -0500

    cli input and helper functions

--- box-packer.php
+++ box-packer.php
@@ -1,23 +1,49 @@
 <?php
-$order = array("40", array("quantity", 5.0, "volume", 12000.0), "33", array("quantity", 6.0, "volume", 2500.0), "35", array("quantity", 3.0, "volume", 1500.0), "41", array("quantity", 1.0, "volume", 1500.0), "34", array("quantity", 3.0, "volume", 500.0), "45", array("quantity", 1.0, "volume", 500.0));
+$created_order = array(
 
-$args = array_shift($argv);
-
-function orderFilled($o) {
-    $array = array();
-    foreach ($o as $ids) {
-        $array[] = $ids
-    }
-    $isZero = Func(function ($ele = null) {
-        if ($ele === 0.0) {
-            return true;
-        }
-    });
-    if (is(call_method($array, "every", $isZero))) {
-        return true;
-    } else {
-        return false;
+);
+echo "\nPlease enter the product id follwed my the quantity required for this order then the volume of one product (ex: 40 3 12000)\nHow many item types are in your order?:";
+$handle = fopen("php://stdin", "r");
+$line = fgets($handle);
+$order_size = $line;
+    while (count($created_order) < (int)$order_size) {
+        echo "Item(id quantity volume):";
+        $line = fgets($handle);
+        $entry = explode(" ", $line);
+        $created_order[$entry[0]] = array(
+        'quantity' => (int)$entry[1],
+        'volume' => (int)$entry[2]
+    );
     };
+
+fclose($handle);
+echo "\n";
+echo "Sorting boxes...\n";
+var_dump($created_order);
+
+function order_filled($o){
+    foreach ($o as $ids) {
+        if ($ids['quantity'] != 0) {
+            return false;
+        };
+    };
+    return true;
 };
 
-echo (orderFilled($order));
+function smallest_volume($o){
+    $volumes = array();
+    foreach ($o as $ids) {
+        if ($ids['quantity'] > 0) {
+            array_push($volumes, $ids['volume']);
+        };
+    };
+    return min($volumes);
+};
+
+function box_full($max, $box, $min) {
+    if ($box['vol'] + $min > $max) {
+        return true;
+    };
+    return false;
+};
+?>
